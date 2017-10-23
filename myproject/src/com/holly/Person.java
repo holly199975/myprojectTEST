7828afc994ba5b060d0c717725d2433322a26982
Holly Deng <holly199975@gmail.com>
2017-10-23 17:29:24 +0800

    initial commit

---
+++ myproject/src/com/holly/Person.java
@@ -0,0 +1,12 @@
+package com.holly;
+
+public class Person {
+
+	public void sayHello(){
+		    System.out.println("hello");
+	}
+	public static void main(String[] args) {
+	Person p = new Person();
+	p.sayHello();
+	}
+}
